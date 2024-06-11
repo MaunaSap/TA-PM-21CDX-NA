afe77c0fd4274a1f514e7528d5672eed85a3da97
MaunaSap <saputramauna@gmail.com>
2024-06-11 18:32:00 +0700

    master:Upload Folder Program dan Dokumentasi

---
+++ PROGRAM/app/src/main/java/com/example/ta_pm_21cdx_na/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.ta_pm_21cdx_na
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.ta_pm_21cdx_na.ui.theme.TAPM21CDXNATheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TAPM21CDXNATheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TAPM21CDXNATheme {
+        Greeting("Android")
+    }
+}
